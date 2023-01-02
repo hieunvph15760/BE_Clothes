110a30cd2f3abbaafd591751ae280c4865613f0e
nguyenvanhieu <hieunvph15760@fpt.edu.vn>
2023-01-02 15:21:39 +0700

    update file model

--- src/models/products.js
+++ src/models/products.js
@@ -1,39 +1,42 @@
 import mongoose from "mongoose";
 const { ObjectId } = mongoose.Type;
 
-const Products = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const Products = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: ObjectId,
+      ref: "Category",
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-  },
-  status: {
-    type: Number,
-    required: true,
-  },
-  image: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: ObjectId,
-    ref: "Category",
-  },
-  quantity: {
-    type: Number,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("products", Products);
